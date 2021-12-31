64f47f7ab102117c3f3be26706643e7c3c00ada6
Zhou Jing <jing.zhou@terapines.com>
2021-12-31 13:12:52 +0800

    Add author info

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,6 @@
-/* eslint-disable jsx-quotes */
+import Taro from "@tarojs/taro";
 import { View } from "@tarojs/components"
-import { AtTimeline } from 'taro-ui'
+import { AtTimeline, AtList, AtListItem } from 'taro-ui'
 import LoginButton from "./loginButton";
 
 const LoginComponent = () => {
@@ -10,16 +10,24 @@ const LoginComponent = () => {
                 项目简介
             </View>
             <View className='at-article__info'>
-                起始于2021/12/30，宿舍，微凉！！
+                起始于2021/12/30，宿舍，微凉！！本项目是周晶同志练手微信小程序用，主要是做一个简单的音乐播放器
+
             </View>
+            <AtList>
+                <AtListItem title='技术栈: React + taro' />
+                <AtListItem title='周晶资料 q1' onClick={() => Taro.navigateTo({
+                    url: '../index/index'
+                })}
+                />
+            </AtList>
             <AtTimeline
               items={[
-                    { title: '查看项目简介',  color: 'red', icon:'check-circle' },
-                    { title:'登陆账号', color: 'blue', icon:'check-circle'  },
-                    { title: '开始听歌', color:'red', icon:'check-circle'  }
+                    { title: '查看项目简介', color: 'red', icon: 'check-circle' },
+                    { title: '登陆账号', color: 'red', icon: 'check-circle' },
+                    { title: '开始听歌', color: 'red', icon: 'check-circle' }
                 ]}
             >
-            </AtTimeline>
+            </AtTimeline> 
             <LoginButton></LoginButton>
         </View>
     )
